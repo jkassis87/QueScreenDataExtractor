b2cee8d26cc5d3dc379010d488c3e9c744de5231
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-01-14 23:10:22 +1100

    ticket data in a clean list thanks to json+flatten

--- qsde.py
+++ qsde.py
@@ -1,14 +1,27 @@
 import requests, json
+from flatten_dict import flatten
 
 ### 1 - import ticket data into script ###
-
+# flatten will take care of the multidimentional dict problems
 # need to convert time from unix to normal, then sydney time, then account for daylight savings
 
+# username and password for the ticket history page
 ruser = 'X'
 rpass = '5rsMThTeZ22p3MqGpz2xRPGY5hAWrwmx'
 
+# retrieves ticket history and loads json data as python dict
+# need to have url appended with date so it get's the previous day's stats each time it runs (at 1am)
 getticketdata = requests.get('http://pbx02.apdcsy1.digitalpacific.com.au/tickethistory_api.php', auth=(ruser, rpass))
-ticketdata = json.loads(getticketdata.txt)
+ticketdata = json.loads(getticketdata.text)
+
+# flattens the 3d dict into a 1d dict
+ticketdata = flatten(ticketdata)
+
+# creates a list of just the ticket stats
+ticketvalue = []
+for idx, itm in ticketdata.items():
+    ticketvalue.append(itm)
+print(ticketvalue)
 
 ### 2 - convert json to list ###
 
