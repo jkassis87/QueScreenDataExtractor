6cd3e4f68d6c923c5dffff6ad58a9eb18931bfbe
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-01-18 08:10:51 +1100

    Update qsde.py

--- qsde.py
+++ qsde.py
@@ -65,16 +65,19 @@ worksheet = sh.get_worksheet(str(daynow))
 r = 2
 c = 2
 tcount = 0
+ttimer = 0
 for x in ticketvalue:
     if tcount < 11:
         worksheet.update_cell(r, c, x)
         r += 1
         tcount += 1
+        ttimer += 1
     elif tcount == 11:
         r = 2
         c += 1
         worksheet.update_cell(r, c, x)
         tcount = 0
+        ttimer += 1
 
 # potentially email people saying script was ran succesfully, or they can check themselves???
 
