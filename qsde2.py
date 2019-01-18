9a71700434c36db7a5400b6836341c594623ecaf
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-01-19 00:22:03 +1100

    added notes and ticket url

--- qsde2.py
+++ qsde2.py
@@ -1,14 +1,25 @@
-import json
+import json, requests
 
-f = open('tdata.json', 'r')
-j = json.load(f)
+#imports json and loads it as dict
+#user/pass for the ticket history api
+ruser = 'X'
+rpass = '5rsMThTeZ22p3MqGpz2xRPGY5hAWrwmx'
 
+#grabs the ticket data and converts it from json to python dict
+### need to modify url, create string using datetime to get previos day
+getticketdata = requests.get('http://pbx02.apdcsy1.digitalpacific.com.au/tickethistory_api.php', auth=(ruser, rpass))
+j = json.loads(getticketdata.text)
+
+#makes list with default values
+###need to add list of hours
 tlist = [['DPL1'], ['DPL2'], ['DPL3'], ['DPLB'],
          ['CRL1'], ['CRL2'], ['CRL3'], ['CRLB'],
          ['PAL1'], ['PAL2'], ['PAL3'], ['PALB'],
          ['TTL1'], ['TTL2'], ['TTL3'], ['TTLB'],
-         ['DFL1'], ['DFL2'], ['DFL3'], ['DFLB']]
+         ['DFL1', 0], ['DFL2', 0], ['DFL3', 0], ['DFLB', 0]]
 
+
+#populates list with ticket stats from json
 for idx, val in j.items():
     #DPL1.append(val[
     tlist[0].append(int(val['DP']['L1']))
@@ -24,12 +35,31 @@ for idx, val in j.items():
     tlist[10].append(int(val['Panthur']['L3']))
     tlist[11].append(int(val['Panthur']['Bil']))
 
+#calculates totals, appends to list
 count = 1
-while count < 13:
+while count < 25:
     tlist[12].append(tlist[0][count] + tlist[4][count] + tlist[8][count])
     tlist[13].append(tlist[1][count] + tlist[5][count] + tlist[9][count])
     tlist[14].append(tlist[2][count] + tlist[6][count] + tlist[10][count])
     tlist[15].append(tlist[3][count] + tlist[7][count] + tlist[11][count])
     count += 1
+
+#calculates difference, appends to list
+###need to confirm if last hour gets appended
+x = 1
+while x < 24:
+    z = tlist[12][x] - tlist[12][x + 1]
+    tlist[16].append(z)
+    z = tlist[13][x] - tlist[13][x + 1]
+    tlist[17].append(z)
+    z = tlist[14][x] - tlist[14][x + 1]
+    tlist[18].append(z)
+    z = tlist[15][x] - tlist[15][x + 1]
+    tlist[19].append(z)
+    x += 1
+
+for item in tlist:
+    print(item, end='\n')
+
 
 
