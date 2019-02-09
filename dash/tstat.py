b5f77f6a81863f1956ba0e0031400494fc0226e6
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-02-09 20:00:32 +1100

    fixed colors

--- dash/tstat.py
+++ dash/tstat.py
@@ -19,14 +19,14 @@ app.layout = html.Div(children=[
     dcc.Graph(id = 'L1 Total',
               figure = {
                   'data': [
-                      {'x': tdata[0], 'y': tdata[13], 'type': 'line', 'name': 'Total L1'},
-                      {'x': tdata[0], 'y': tdata[17], 'type': 'bar', 'name': 'Diff L1'},
-                      {'x': tdata[0], 'y': tdata[14], 'type': 'line', 'name': 'Total L2'},
-                      {'x': tdata[0], 'y': tdata[18], 'type': 'bar', 'name': 'Diff L2'},
-                      {'x': tdata[0], 'y': tdata[15], 'type': 'line', 'name': 'Total L3'},
-                      {'x': tdata[0], 'y': tdata[19], 'type': 'bar', 'name': 'Diff L3'},
-                      {'x': tdata[0], 'y': tdata[16], 'type': 'line', 'name': 'Total Bil'},
-                      {'x': tdata[0], 'y': tdata[20], 'type': 'bar', 'name': 'Diff Bil'},
+                      {'x': tdata[0], 'y': tdata[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+                      {'x': tdata[0], 'y': tdata[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+                      {'x': tdata[0], 'y': tdata[14], 'type': 'line', 'name': 'Total L2', 'marker': {'color': 'rgb(234, 0, 0)'}},
+                      {'x': tdata[0], 'y': tdata[18], 'type': 'bar', 'name': 'Diff L2', 'marker': {'color': 'rgb(234, 0, 0)'}},
+                      {'x': tdata[0], 'y': tdata[15], 'type': 'line', 'name': 'Total L3', 'marker': {'color': 'rgb(0, 234, 15)'}},
+                      {'x': tdata[0], 'y': tdata[19], 'type': 'bar', 'name': 'Diff L3', 'marker': {'color': 'rgb(0, 234, 15)'}},
+                      {'x': tdata[0], 'y': tdata[16], 'type': 'line', 'name': 'Total Bil', 'marker': {'color': 'rgb(246, 0, 255)'}},
+                      {'x': tdata[0], 'y': tdata[20], 'type': 'bar', 'name': 'Diff Bil', 'marker': {'color': 'rgb(246, 0, 255)'}},
                       ],
                   'layout': {
                       'title': 'Hostopia Ticket Stats',
