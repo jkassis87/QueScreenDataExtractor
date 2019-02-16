bb231c0e7cde1db34472aa61e0bc17449e8c0f2c
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-02-16 19:22:12 +1100

    calendar almost working
    Works if 0 is in the month

--- dash/tstat.py
+++ dash/tstat.py
@@ -5,60 +5,61 @@ from dash.dependencies import Input, Output
 import csv
 from datetime import datetime as dt
 from datetime import timedelta
-
-now = dt.now() - timedelta(1)
-date = (str(now.year) + '-' + str(now.month) + '-' + str(now.day))
-
-def get_csv(datestring):
-    f = open((datestring + '.csv'), 'r')
-    csvfile = csv.reader(f)
-    tdata = list(csvfile)
-    return(tdata)
+import re
 
 app = dash.Dash(__name__)
 
 colors = {
     'background': '#FFFFFF',
-    'text': '#111111'
 }
 
 app.layout = html.Div(children=[
     dcc.DatePickerSingle(
         id = 'callendar-1',
-        min_date_allowed = dt(2019, 1, 11),
-        max_date_allowed = now,
-        initial_visible_month = now,
-        date = dt(2019, 1, 12)),
-    html.Div(id='callendar-output'),
-    dcc.Graph(id = 'L1 Total',
-              figure = {
-                  'data': [
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[14], 'type': 'line', 'name': 'Total L2', 'marker': {'color': 'rgb(234, 0, 0)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[18], 'type': 'bar', 'name': 'Diff L2', 'marker': {'color': 'rgb(234, 0, 0)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[15], 'type': 'line', 'name': 'Total L3', 'marker': {'color': 'rgb(0, 234, 15)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[19], 'type': 'bar', 'name': 'Diff L3', 'marker': {'color': 'rgb(0, 234, 15)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[16], 'type': 'line', 'name': 'Total Bil', 'marker': {'color': 'rgb(246, 0, 255)'}},
-                      {'x': get_csv(date)[0], 'y': get_csv(date)[20], 'type': 'bar', 'name': 'Diff Bil', 'marker': {'color': 'rgb(246, 0, 255)'}},
-                      ],
-                  'layout': {
-                      'title': 'Hostopia Ticket Stats',
-                      'plot_bgcolor': colors['background'],
-                      }
-                  }
+        min_date_allowed = dt(2019, 1, 15),
+        max_date_allowed = dt(2019, 2, 16),
+        initial_visible_month = dt(2019, 2, 16),
+        date = dt(2019, 2, 15)),
+    html.Div(id = 'callendar-output'),
+    dcc.Graph(id = 'Daily-Ticket-Stats',
+#              figure = {
+#                  'data': [
+#                      {'x': get_csv(date)[0], 'y': get_csv(date)[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+#                      {'x': get_csv(date)[0], 'y': get_csv(date)[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+#                      ],
+#                  'layout': {
+#                      'title': 'Ticket Stats',
+#                      'plot_bgcolor': colors['background'],
+#                      }
+#                  }
               )
 ])
 
 @app.callback(
-    dash.dependencies.Output('callendar-output', 'children'),
-    [dash.dependencies.Input('callendar-1', 'date')])
+    dash.dependencies.Output('Daily-Ticket-Stats', 'figure'),
+    [dash.dependencies.Input('callendar-1', 'date')]
+    )
 def update_output(date):
+
     if date is not None:
-        date = dt.strptime(date, '%Y-%m-%d')
-        date_string = date.strftime('%Y-%m-%e')
-        opencsv = date_string + ".csv"
-        return date_string + ".csv"
+        thedate = dt.strptime(date, '%Y-%m-%d')
+        date_string = str(thedate)
+        date_string = re.sub(' 00:00:00', '', date_string)
+        f = open((date_string + '.csv'))
+        csvfile = csv.reader(f)
+        tdata = list(csvfile)
+        figure = {
+            'data': [
+                      {'x': tdata[0], 'y': tdata[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+                      {'x': tdata[0], 'y': tdata[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+                      ],
+            'layout': {
+                      'title': 'Ticket Stats',
+                      'plot_bgcolor': colors['background'],
+                      }
+                  }
+    return figure
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
