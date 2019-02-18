a8f73d900edfcd54e164108d44d11ff436d57478
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-02-18 21:31:41 +1100

    tabs 1 and 2 fully working with all dept stats

--- dash/tstat.py
+++ dash/tstat.py
@@ -30,19 +30,13 @@ app.layout = html.Div([
             ])
         ]),
         dcc.Tab(label='Compare 2 Days', children=[
-            dcc.DatePickerSingle(
+            dcc.DatePickerRange(
                 id='cal-tab2A',
                 min_date_allowed=dt(2019, 1, 15),
                 max_date_allowed=dt(2019, 2, 16),
                 initial_visible_month=dt(2019, 2, 16),
-                date=dt(2019, 2, 15),
-            ),
-            dcc.DatePickerSingle(
-                id='cal-tab2B',
-                min_date_allowed=dt(2019, 1, 15),
-                max_date_allowed=dt(2019, 2, 16),
-                initial_visible_month=dt(2019, 2, 16),
-                date=dt(2019, 2, 15),
+                start_date=dt(2019, 1, 15),
+                end_date=dt(2019, 8, 25)
             ),
                 dcc.Graph(
                     id='graph-tab2',
@@ -54,6 +48,7 @@ app.layout = html.Div([
                 min_date_allowed=dt(2019, 1, 15),
                 max_date_allowed=dt(2019, 2, 17),
                 initial_visible_month=dt(2019, 2, 1),
+                start_date= dt(2019, 1, 15),
                 end_date=dt(2019, 8, 25),
             ),
                 dcc.Graph(
@@ -66,6 +61,7 @@ app.layout = html.Div([
                 min_date_allowed=dt(2019, 1, 15),
                 max_date_allowed=dt(2019, 2, 17),
                 initial_visible_month=dt(2019, 2, 1),
+                start_date=dt(2019, 1, 15),
                 end_date=dt(2019, 8, 25),
             ),
             dcc.DatePickerRange(
@@ -73,6 +69,7 @@ app.layout = html.Div([
                 min_date_allowed=dt(2019, 1, 15),
                 max_date_allowed=dt(2019, 2, 17),
                 initial_visible_month=dt(2019, 2, 1),
+                start_date=dt(2019, 1, 15),
                 end_date=dt(2019, 8, 25),
             ),
                 dcc.Graph(
@@ -100,8 +97,14 @@ def update_tab1(date):
         tdata = list(csvfile)
         figure = {
             'data': [
-                      {'x': tdata[0], 'y': tdata[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
-                      {'x': tdata[0], 'y': tdata[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+                      {'x': tdata[0], 'y': tdata[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 0, 255)'}},
+                      {'x': tdata[0], 'y': tdata[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 0, 255)'}},
+                {'x': tdata[0], 'y': tdata[14], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(255, 0, 0)'}},
+                {'x': tdata[0], 'y': tdata[18], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(255, 0, 0)'}},
+                {'x': tdata[0], 'y': tdata[16], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 255, 0)'}},
+                {'x': tdata[0], 'y': tdata[20], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 255, 0)'}},
+                {'x': tdata[0], 'y': tdata[15], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(51, 51, 51)'}},
+                {'x': tdata[0], 'y': tdata[19], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(51, 51, 51)'}},
                       ],
             'layout': {
                       'title': 'Ticket Stats',
@@ -115,22 +118,43 @@ def update_tab1(date):
 # callback for tab 2
 @app.callback(
     dash.dependencies.Output('graph-tab2', 'figure'),
-    [dash.dependencies.Input('cal-tab2A', 'date'),
+    [dash.dependencies.Input('cal-tab2A', 'start_date'),
+    dash.dependencies.Input('cal-tab2A', 'end_date')
      ]
     )
-def update_tab2a(date):
+def update_tab2a(start_date, end_date):
 
-    if date is not None:
-        thedate = dt.strptime(date, '%Y-%m-%d')
-        date_string = str(thedate)
-        date_string = re.sub(' 00:00:00', '', date_string)
-        f = open((date_string + '.csv'))
-        csvfile = csv.reader(f)
-        tdata = list(csvfile)
+    if start_date is not None and end_date is not None:
+        thedate_start = dt.strptime(start_date, '%Y-%m-%d')
+        thedate_end = dt.strptime(end_date, '%Y-%m-%d')
+        date_string_start = str(thedate_start)
+        date_string_end = str(thedate_end)
+        date_string_start = re.sub(' 00:00:00', '', date_string_start)
+        date_string_end = re.sub(' 00:00:00', '', date_string_end)
+        f_start = open((date_string_start + '.csv'))
+        f_end = open((date_string_end + '.csv'))
+        csvfile_start = csv.reader(f_start)
+        csvfile_end = csv.reader(f_end)
+        tdata_start = list(csvfile_start)
+        tdata_end = list(csvfile_end)
         figure = {
             'data': [
-                      {'x': tdata[0], 'y': tdata[13], 'type': 'line', 'name': 'Total L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
-                      {'x': tdata[0], 'y': tdata[17], 'type': 'bar', 'name': 'Diff L1', 'marker': {'color': 'rgb(0, 89, 234)'}},
+                      {'x': tdata_start[0], 'y': tdata_start[13], 'type': 'line', 'name': 'Total L1 - A', 'marker': {'color': 'rgb(0, 0, 255)'}},
+                      {'x': tdata_start[0], 'y': tdata_start[17], 'type': 'bar', 'name': 'Diff L1 - A', 'marker': {'color': 'rgb(0, 0, 255)'}},
+                {'x': tdata_end[0], 'y': tdata_end[13], 'type': 'line', 'name': 'Total L1 - B', 'marker': {'color': 'rgba(0, 0, 255, 0.6)'}},
+                {'x': tdata_end[0], 'y': tdata_end[17], 'type': 'bar', 'name': 'Diff L1 - B', 'marker': {'color': 'rgba(0, 0, 255, 0.6)'}},
+                {'x': tdata_start[0], 'y': tdata_start[14], 'type': 'line', 'name': 'Total L2 - A', 'marker': {'color': 'rgb(255, 0, 0)'}},
+                {'x': tdata_start[0], 'y': tdata_start[18], 'type': 'bar', 'name': 'Diff L2 - A', 'marker': {'color': 'rgb(255, 0, 0)'}},
+                {'x': tdata_end[0], 'y': tdata_end[14], 'type': 'line', 'name': 'Diff L2 - B', 'marker': {'color': 'rgba(255, 0, 0, 0.6)'}},
+                {'x': tdata_end[0], 'y': tdata_end[18], 'type': 'bar', 'name': 'Diff L2 - B', 'marker': {'color': 'rgba(255, 0, 0, 0.6)'}},
+                {'x': tdata_end[0], 'y': tdata_end[16], 'type': 'bar', 'name': 'Total BL - A', 'marker': {'color': 'rgb(0, 255, 0)'}},
+                {'x': tdata_start[0], 'y': tdata_start[20], 'type': 'line', 'name': 'Diff BL - A', 'marker': {'color': 'rgb(0, 255, 0)'}},
+                {'x': tdata_start[0], 'y': tdata_start[16], 'type': 'bar', 'name': 'Total BL - B', 'marker': {'color': 'rgba(0, 255, 0, 0.6)'}},
+                {'x': tdata_end[0], 'y': tdata_end[20], 'type': 'line', 'name': 'Diff BL - B', 'marker': {'color': 'rgba(0, 255, 0, 0.6)'}},
+                {'x': tdata_start[0], 'y': tdata_start[15], 'type': 'line', 'name': 'Total L3 - A', 'marker': {'color': 'rgb(51, 51, 51)'}},
+                {'x': tdata_start[0], 'y': tdata_start[19], 'type': 'bar', 'name': 'Diff L3 - A', 'marker': {'color': 'rgb(51, 51, 51)'}},
+                {'x': tdata_end[0], 'y': tdata_end[15], 'type': 'line', 'name': 'Total L3 - B', 'marker': {'color': 'rgba(51, 51, 51, 0.6)'}},
+                {'x': tdata_end[0], 'y': tdata_end[19], 'type': 'bar', 'name': 'Diff L3 - B', 'marker': {'color': 'rgba(51, 51, 51, 0.6)'}}
                       ],
             'layout': {
                       'title': 'Ticket Stats',
