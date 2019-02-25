72fffe86770febd25768ccf82b21b0616c556df1
jkassis87 <44606645+jkassis87@users.noreply.github.com>
2019-02-25 22:31:09 +1100

    DB now working, no total or diff data yet

--- sqlite/gettdata.py
+++ sqlite/gettdata.py
@@ -1,35 +1,40 @@
 import sqlite3, json, requests, csv
 from datetime import datetime, timedelta, date
-from os.path import isdir
-from os import mkdir, makedirs
+
+now = datetime.now() - timedelta(1)
+nowst = datetime.strftime(now, '%Y-%m-%d')
+yesterday = (datetime.strftime(now, '%Y') + '-' + datetime.strftime(now, '%m') + '-' + datetime.strftime(now, '%d'))
+dayname = now.strftime("%A")
 
 
 # user/pass and URL for the ticket history api
 ruser = 'X'
 rpass = '5rsMThTeZ22p3MqGpz2xRPGY5hAWrwmx'
 urltoget = (r'http://pbx02.apdcsy1.digitalpacific.com.au/tickethistory_api.php?date=2019-1-15')
-
-# grabs the ticket data and converts it from json to python dict
 getticketdata = requests.get(urltoget, auth=(ruser, rpass))
 j = json.loads(getticketdata.text)
-tlist = [[], [], [], [], [], [], [], [], [], [], [], []]
 
+thour = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+tlist = []
+
+x = 0
 for idx, val in j.items():
-    tlist[0].append(int(val['DP']['L1']))
-    tlist[1].append(int(val['DP']['L2']))
-    tlist[2].append(int(val['DP']['L3']))
-    tlist[3].append(int(val['DP']['Bil']))
-    tlist[4].append(int(val['Crucial']['L1']))
-    tlist[5].append(int(val['Crucial']['L2']))
-    tlist[6].append(int(val['Crucial']['L3']))
-    tlist[7].append(int(val['Crucial']['Bil']))
-    tlist[8].append(int(val['Panthur']['L1']))
-    tlist[9].append(int(val['Panthur']['L2']))
-    tlist[10].append(int(val['Panthur']['L3']))
-    tlist[11].append(int(val['Panthur']['Bil']))
+    tlist.append(['L1', 'DP', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L1'])])
+    tlist.append(['L2', 'DP', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L2'])])
+    tlist.append(['L3', 'DP', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L3'])])
+    tlist.append(['BL', 'DP', yesterday, dayname, thour[x], 'tcount', int(val['DP']['Bil'])])
+    tlist.append(['L1', 'CR', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L1'])])
+    tlist.append(['L2', 'CR', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L2'])])
+    tlist.append(['L3', 'CR', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L3'])])
+    tlist.append(['BL', 'CR', yesterday, dayname, thour[x], 'tcount', int(val['DP']['Bil'])])
+    tlist.append(['L1', 'PA', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L1'])])
+    tlist.append(['L2', 'PA', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L2'])])
+    tlist.append(['L3', 'PA', yesterday, dayname, thour[x], 'tcount', int(val['DP']['L3'])])
+    tlist.append(['BL', 'PA', yesterday, dayname, thour[x], 'tcount', int(val['DP']['Bil'])])
+    x += 1
 
 
-sqlite_file = 'tdata.sqlite'    # name of the sqlite database file
+sqlite_file = 'tdata2.sqlite'    # name of the sqlite database file
 
 
 # Connecting to the database file
@@ -37,35 +42,20 @@ conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # Creating a new SQLite table with 1 column
-c.execute(r'''CREATE TABLE JanOne(
-DPL1 Int,
-DPL2 Int,
-DPL3 Int,
-DPBL Int,
-CRL1 Int,
-CRL2 Int,
-CRL3 Int,
-CRBL Int,
-PAL1 Int,
-PAL2 Int,
-PAL3 Int,
-PABL Int,
-TTL1 Int,
-TTL2 Int,
-TTL3 Int,
-TTBL Int,
-DFL1 Int,
-DFL2 Int,
-DFL3 Int,
-DFBL Int
+c.execute(r'''CREATE TABLE AllData(
+Team Text,
+Brand Text,
+Date Text,
+Day Text,
+Hour Int,
+Type Text,
+Stat Int
 );
 ''')
 
-for i in range(len(tlist)):
-    c.execute("INSERT INTO JanOne (DPL1, DPL2, DPL3, DPBL, CRL1, CRL2, CRL3, CRBL, PAL1, PAL2, PAL3, PABL,)"
-              " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-              (tlist[0][i], tlist[1][i], tlist[2][i], tlist[3][i], tlist[4][i], tlist[5][i], tlist[6][i], tlist[7][i], tlist[8][i], tlist[9][i], tlist[10][i], tlist[11][i],))
+for x in tlist:
+    c.execute('INSERT INTO AllData VALUES (?,?,?,?,?,?,?)', x)
 
-# Committing changes and closing the connection to the database file
 conn.commit()
 conn.close()
+
